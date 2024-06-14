ba0d982453155c082d902d72fb3869aa45a76c2e
Jamyan <148853505+Jamyan-1234@users.noreply.github.com>
2024-06-14 12:07:11 +0800

    Co-authored-by: sk4walker <sk4walker@users.noreply.github.com>

--- Dadlaga/src/Pages/login.jsx
+++ Dadlaga/src/Pages/login.jsx
@@ -20,53 +20,79 @@ function Login() {
         <img className='for_image' src={desk} height={680} width={900} alt="My Image" />
         </div>
 
-        <div className='bruh'>
+      
 
-        </div>
+        
 
         
         
-        {/* 
+        {/* */}
         <div className="signup-containerr">
-
+          <Link to="/" className="back-link">← Буцах</Link>
           <div className='first_div'>
-           
             <h2> Тавтай морил</h2>
           </div>
-          
           <h1>Нэвтрэх</h1>
-
-
           
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <input
-                type="email"
-                id="email"
-                placeholder="Email Хаяг"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className="form-group">
+          <div className='textbox_container'>
+            <form className='m' onSubmit={handleSubmit}>
+              <div className='textbox_size'>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Email Хаяг"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className='textbox_size'>
+                
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Нууц үг"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
               
-              <input
-                type="password"
-                id="password"
-                placeholder="Нууц үг"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            
-            
-            
-            <button type="submit" className="signup-button">Нэвтрэх</button>
-          </form>
+              
+              
+              
+            </form>
+          </div>
           
-        </div>*/}
+          <div className='button_div'>
+          <button type="submit" className="signup-buttonn">Нэвтрэх</button>
+          <Link to="/">Нууц үгээ мартсан уу?</Link>
+          </div>
+         
+          
+
+
+
+
+
+
+
+
+          
+          
+         
+          
+
+          
+
+
+          
+            
+            
+         
+          
+        </div>
       </div>
       
 
