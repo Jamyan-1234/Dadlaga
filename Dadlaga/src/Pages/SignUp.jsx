02a300d866a5a6b0d22dc36621cfc69e196eda55
Jamyan <148853505+Jamyan-1234@users.noreply.github.com>
2024-06-16 13:07:32 +0800

    Бүртгүүлэх нүүр хуудас бүрэн дуусав

--- Dadlaga/src/Pages/SignUp.jsx
+++ Dadlaga/src/Pages/SignUp.jsx
@@ -21,7 +21,7 @@ function SignUp() {
         </div>
 
         <div className="signup-container">
-          <Link to="/" className="back-link">← Буцах</Link>
+         
           <h1>Бүртгүүлэх</h1>
 
 
@@ -48,7 +48,7 @@ function SignUp() {
               />
             </div>
             <div className="form-group">
-              <label htmlFor="confirmPassword">Нууц үг давтах *</label>
+              <label htmlFor="confirmPassword">Нууц үгээ давтан оруулна уу *</label>
               <input
                 type="password"
                 id="confirmPassword"
@@ -62,19 +62,30 @@ function SignUp() {
               <p>Том ба жижиг үсэг холилдсон</p>
               <p>8 болон түүнээс дээш оронтой</p>
             </div>
-            <div className="form-group">
-              <input
-                type="checkbox"
-                id="agree"
-                checked={agreed}
-                onChange={(e) => setAgreed(e.target.checked)}
-                required
-              />
-              <label htmlFor="agree">Нууцлалын бодлогыг хүлээн зөвшөөрч байна.</label>
+
+            <div className='checkbox_container'>
+
+              <div className="check_box_and_text_same_level">
+                <input
+                  type="checkbox"
+                  id="agree"
+                  checked={agreed}
+                  onChange={(e) => setAgreed(e.target.checked)}
+                  required
+                />
+                <label htmlFor="agree">Нууцлалын бодлогыг хүлээн зөвшөөрч байна.</label>
+                
+              </div>
             </div>
-            <button type="submit" className="signup-button">Бүртгүүлэх</button>
+
+            
+            
           </form>
-          <p className="login-link">Бүртгэлтэй юу? <Link to="/login">Нэвтрэх</Link></p>
+          <div className='sign_up_button_container'>
+            <button type="submit" className="signup-button">Бүртгүүлэх</button>
+            <p className="login-link">Бүртгэлтэй юу? <Link to="/login">Нэвтрэх</Link></p>
+          </div>
+          
         </div>
       </div>
       
