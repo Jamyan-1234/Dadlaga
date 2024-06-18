f8f5c491798742c2ffe2b3f0fd4dc5edabb6c668
Jamyan <148853505+Jamyan-1234@users.noreply.github.com>
2024-06-19 00:52:31 +0800

    qwerty

--- Dadlaga/src/Pages/SignUp.jsx
+++ Dadlaga/src/Pages/SignUp.jsx
@@ -1,22 +1,42 @@
-import React, { useState } from 'react';
+
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../CSS_files/SignUp.css';
 import desk from '../images/image5.jpg';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+
 
 function SignUp() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [agreed, setAgreed] = useState(false);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    axios.post( 'http://localhost:3001/login', {email, password})
+    .then(result => {
+        console.log(result);
+        if(result.data === "Success"){
+            console.log("Login Success");
+            alert('Login successful!')
+            navigate('/home');
+        }
+        else{
+            alert('Incorrect password! Please try again.');
+        }
+    })
+    .catch(err => console.log(err));
+  }
+  
 
 
 
   
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
+  
 
   return (
     <div className='main_container'>
@@ -34,41 +54,34 @@ function SignUp() {
           <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="email">И-мэйл *</label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
+              <input 
+                                type="email" 
+                                placeholder="Enter Email"
+                                className="form-control" 
+                                id="exampleInputEmail1" 
+                                onChange={(event) => setEmail(event.target.value)}
+                                required
+                            />
             </div>
             
             <div className="form-group">
               <label htmlFor="password">Нууц үг *</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
+              <input 
+                                type="password" 
+                                placeholder="Enter Password"
+                                className="form-control" 
+                                id="exampleInputPassword1" 
+                                onChange={(event) => setPassword(event.target.value)}
+                                required
+                            />
             </div>
-            <div className="form-group">
-              <label htmlFor="confirmPassword">Нууц үгээ давтан оруулна уу *</label>
-              <input
-                type="password"
-                id="confirmPassword"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-              />
-            </div>
+            
             <div className="password-requirements">
               <p>Тоо агуулсан</p>
               <p>Том ба жижиг үсэг холилдсон</p>
               <p>8 болон түүнээс дээш оронтой</p>
             </div>
-
+{/*
             <div className='checkbox_container'>
 
               <div className="check_box_and_text_same_level">
@@ -78,17 +91,17 @@ function SignUp() {
                   checked={agreed}
                   onChange={(e) => setAgreed(e.target.checked)}
                   required
-                />
+                /> 
                 <label htmlFor="agree">Нууцлалын бодлогыг хүлээн зөвшөөрч байна.</label>
                 
               </div>
-            </div>
+            </div>*/}
 
             
             
           </form>
           <div className='sign_up_button_container'>
-            <button type="submit" className="signup-button">Бүртгүүлэх</button>
+          <button type="submit" className="signup-button">Бүртгүүлэх</button>
             <p className="login-link">Бүртгэлтэй юу? <Link to="/login">Нэвтрэх</Link></p>
           </div>
           
